aff6d3503cd375f456196f36e3ddbcc7c11f79ea
Jack James <sales@surrealroad.com>
2026-01-31 19:09:38 +0000

    responsive layout

--- demo/src/components/example-component.tsx
+++ demo/src/components/example-component.tsx
@@ -17,20 +17,21 @@ export function ExampleComponent({
 }: ExampleComponentProps) {
   return (
     <div className="grid grid-cols-2 gap-0 items-stretch shadow-retro-lg">
+      {/* Output */}
+      <div className="col-span-2 md:col-span-1 overflow-hidden rounded-none rounded-tr-sm border-2 border-neutral-800 border-b-0">
+        {output}
+      </div>
+
       {/* Controls */}
-      <Card className="bg-background rounded-none rounded-tl-sm border-2 border-neutral-800 border-b-0 border-r-none">
+      <Card className="col-span-2 md:col-span-1 bg-background rounded-none rounded-tl-sm border-2 border-neutral-800 border-b-0 border-r-none">
         <CardHeader>
           <CardTitle>{title}</CardTitle>
         </CardHeader>
         <CardContent className="space-y-4">{controls}</CardContent>
       </Card>
 
-      {/* Output */}
-      <div className="overflow-hidden rounded-none rounded-tr-sm border-2 border-neutral-800 border-b-0">
-        {output}
-      </div>
-
-      <div className="overflow-hidden rounded-none rounded-b-sm col-span-2 border-2 border-neutral-800">
+      {/* Code */}
+      <div className="col-span-2 overflow-hidden rounded-none rounded-b-sm border-2 border-neutral-800">
         <CodeBlock
           className="rounded-none rounded-b-sm"
           code={code}
