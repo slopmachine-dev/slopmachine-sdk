854d30753840968b3ebc9f95d2e591e9d1dbb350
Jack James <sales@surrealroad.com>
2026-02-25 22:12:45 +0000

    fix build issue

--- packages/core/src/index.d.ts
+++
@@ -1,8 +0,0 @@
-export interface SlopMachineOptions {
-    prompt: string;
-    aspectRatio?: string;
-    variables?: Record<string, string | number | undefined | null>;
-    baseUrl?: string;
-}
-export declare function interpolatePrompt(prompt: string, variables?: Record<string, string | number | undefined | null>): string;
-export declare function buildImageUrl(options: SlopMachineOptions): string;
